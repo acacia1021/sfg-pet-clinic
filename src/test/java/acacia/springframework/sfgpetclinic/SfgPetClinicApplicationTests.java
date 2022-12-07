577d95c0024052a6bae4e63eba36fb98c462eab2
acacia1021 <acacia1021@gmail.com>
2022-12-07 09:40:55 -0500

    S.Boot Proj. Created w/added content on .gitignore

---
+++ src/test/java/acacia/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package acacia.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
